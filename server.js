7169a680d1f46517017302bd158a34fe04a22b6e
Cristal Gomez <cristalsg1@gmail.com>
2019-05-06 18:58:42 -0500

    changes to the setup

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
+
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Configure body parser for AJAX requests
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
+// Configure body parsing for AJAX requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // Serve up static assets
-app.use(express.static(path.join(__dirname, 'client/build/')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 // Add routes, both API and view
 app.use(routes);
 
-// Set up promises with mongoose
-mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/iimage");
+  process.env.MONGODB_URI || "mongodb://localhost/iimage",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
 
 // Start the API server
 app.listen(PORT, () =>
