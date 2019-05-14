5b031743b0a34828a2ced328dedf8ce9bdfecf7e
Cristal Gomez <cristalsg1@gmail.com>
2019-05-13 19:08:33 -0500

    login startup/ placed firebase

---
+++ client/src/Config.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyAKCeWfbrL7RP6yLaEFZrK8UAuCmNaSs30",
+    authDomain: "iimageproject.firebaseapp.com",
+    databaseURL: "https://iimageproject.firebaseio.com",
+    projectId: "iimageproject",
+    storageBucket: "iimageproject.appspot.com",
+    messagingSenderId: "254020928797",
+    appId: "1:254020928797:web:c811b5ea32445ab8"
+};
+
+const app = firebase.initializeApp(config)
+const base = Rebase.createClass(app.database())
+
+export { app, base }
