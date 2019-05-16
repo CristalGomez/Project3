676be38d0b087aa4ab1b95e73585e56384271b4b
Piper <skylarpiper710@gmail.com>
2019-05-15 19:12:13 -0500

    upload working, working on front page,

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,8 +1,8 @@
 const card = [{
     "id":1,
     "name": "Skylar Piper",
-    "image":"https://i.imgur.com/sHDSn2Z.jpg",
-    "icon": "https://i.imgur.com/sHDSn2Z.jpg",
+    "image":"https://files.slack.com/files-tmb/TDV67LNC8-FJQT0NA4V-c6e654a39f/sky_720.jpg",
+    "icon": "https://files.slack.com/files-tmb/TDV67LNC8-FJQT0NA4V-c6e654a39f/sky_720.jpg",
     "about":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     "count": 0
 },
--- client/src/components/ImageUpload/ImageUpload.jsx
+++ client/src/components/ImageUpload/ImageUpload.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
-import {storage} from "../../firebase"
+import { storage } from "../../firebase"
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
+import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from 'mdbreact';
+import "./ImageUpload.css"
 
 class ImageUpload extends Component {
     constructor(props) {
@@ -7,39 +11,39 @@ class ImageUpload extends Component {
         this.state = {
             image: null,
             url: '',
-            progress:0,
+            progress: 0,
         }
         this.handleChange = this
-        .handleChange
-        .bind(this);
+            .handleChange
+            .bind(this);
         this.handleUpload = this.handleUpload.bind(this);
     }
     handleChange = e => {
-        if(e.target.files[0]){
+        if (e.target.files[0]) {
             const image = e.target.files[0];
-            this.setState(() => ({image}))
+            this.setState(() => ({ image }))
         }
     }
     handleUpload = () => {
-        const {image} = this.state;
+        const { image } = this.state;
         const uploadTask = storage.ref(`images/${image.name}`).put(image);
         uploadTask.on('state_changed',
-        (snapshot) => {
-            // progress function
-            const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
-            this.setState({progress})
-        },
-        (error)=> {
-            // error function
-            console.log(error);
-        },
-        () => {
-            // complete function
-            storage.ref('images').child(image.name).getDownloadURL().then(url => {
-                console.log(url);
-                this.setState({url});
-            })
-        });
+            (snapshot) => {
+                // progress function
+                const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
+                this.setState({ progress })
+            },
+            (error) => {
+                // error function
+                console.log(error);
+            },
+            () => {
+                // complete function
+                storage.ref('images').child(image.name).getDownloadURL().then(url => {
+                    console.log(url);
+                    this.setState({ url });
+                })
+            });
     }
     render() {
         return (
@@ -48,12 +52,26 @@ class ImageUpload extends Component {
 
             // }
             <div>
-            <progress value={this.state.progress} max='100'/>
-            <br/>
+                {/* <progress value={this.state.progress} max='100' />
+                <br />
                 <input type='file' onChange={this.handleChange} />
                 <button onClick={this.handleUpload}>Upload</button>
-                <br/>
-                <img src={this.state.url || 'https://via.placeholder.com/300x150' } alt='uploaded images' height='300' width='400'/>
+                <br />
+                <img src={this.state.url || 'https://via.placeholder.com/300x150'} alt='uploaded images' height='300' width='400' /> */}
+                <Container>
+                    <Row>
+                        <Col xs='6'>
+                            
+                            <input className="fileInput" type='file' onChange={this.handleChange} />
+                            <br/>
+                            <MDBBtn className="uploadBtn" onClick={this.handleUpload}>Upload</MDBBtn>
+                            <progress className="progressBar w3-grey" value={this.state.progress} max='100' />
+                            {/* <button className="uploadBtn" onClick={this.handleUpload}>Upload</button> */}
+                            <br />
+                            <img src={this.state.url || 'https://via.placeholder.com/300x150'} alt='uploaded images' height='300' width='400' />
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         )
     }
