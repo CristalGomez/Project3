86d6d7e4607d664303650251303472b9888418a3
Cristal Gomez <cristalsg1@gmail.com>
2019-05-15 19:13:40 -0500

    pulling from master to test login and signup

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
-import { App } from '../App'
+import { app, config } from '../../Config'
 
 
 //mine;
@@ -13,32 +13,6 @@ const loginStyles = {
   padding: "10px"
 }
 
-class Login extends Component {
-  render(){
-    return {
-      <div style={loginStyles>
-      <h3>Email</h3>
-      <input style={{width: "100%"}} className="input-email" name="email" type="email" ref={(input) => {this.emailInput = input}} placeholder="Email"></input>
-
-      <h3>Password</h3>
-      <input style={{width: "100%"}} className="input-password" name="password" type="password" ref={(input) => {this.passwordInput = input}} placeholder="Password"></input>
-      </div>
-
-    }
-  }
-}
-
-
-//theirs:
-
-const loginStyles = {
-  width: "90%",
-  maxWidth: "315px",
-  margin: "20px auto",
-  border: "1px solid #ddd",
-  borderRadius: "5px",
-  padding: "10px"
-}
 
 class Login extends Component {
   constructor(props) {
@@ -60,10 +34,6 @@ class Login extends Component {
         if (providers.length === 0) {
           // create user
           return app.auth().createUserWithEmailAndPassword(email, password)
-        } else if (providers.indexOf("password") === -1) {
-          // they used facebook
-          this.loginForm.reset()
-          this.toaster.show({ intent: Intent.WARNING, message: "Try alternative login." })
         } else {
           // sign user in
           return app.auth().signInWithEmailAndPassword(email, password)
@@ -77,7 +47,7 @@ class Login extends Component {
         }
       })
       .catch((error) => {
-        this.toaster.show({ intent: Intent.DANGER, message: error.message })
+        res.status(422).json(err) 
       })
   }
 
@@ -92,11 +62,7 @@ class Login extends Component {
       <div style={loginStyles}>
         <hr style={{marginTop: "10px", marginBottom: "10px"}}/>
         <form onSubmit={(event) => { this.authWithEmailPassword(event) }} ref={(form) => { this.loginForm = form }}>
-          <div style={{marginBottom: "10px"}} className="pt-callout pt-icon-info-sign">
-            <h5>Note</h5>
-            If you don't have an account already, this form will create your account.
-          </div>
-          <label className="pt-label">
+          <label className="email-lable">
             Email
             <input style={{width: "100%"}} className="pt-input" name="email" type="email" ref={(input) => { this.emailInput = input }} placeholder="Email"></input>
           </label>
