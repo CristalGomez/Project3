3fc566264fa4394e964da840149b4006245e9742
Cristal Gomez <cristalsg1@gmail.com>
2019-05-17 10:00:27 -0500

    signup page setup to capture user input & save to okta

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import folderApi from '../../utils/foldersAPI';
 import { Link } from 'react-router-dom';
 import { Image } from '../../components/Image'
+import { withAuth } from '@okta/okta-react'
 
 import {
   Carousel,
@@ -115,3 +116,56 @@ class XPage extends Component {
 }
 
 export default XPage;
+
+
+export default withAuth(class Home extends Component {
+  state = { authenticated: null};
+
+  checkAuthentication = async () => {
+      const authenticated = await this.props.auth.isAuthenticated();
+      if(authenticated !== this.state.auth){
+          this.setState({ authenticated})
+      }
+  }
+
+  async componentDidMount(){
+      this.checkAuthentication()
+  }
+
+  async componentDidUpdate(){
+      this.checkAuthentication()
+  }
+
+  login = async () => {
+      this.props.auth.login('/')
+  }
+
+  logout = async () => {
+      this.props.auth.logout('/')
+  }
+
+  render(){
+      if (this.state.authenticated === null) return null;
+
+      const button = this.state.authenticated ? (
+          //displayed in the Navbar of profile page
+          <div>
+          
+              <button className="logout-btn" onClick={this.logout}> Logout</button>
+          </div>
+      ) : (
+          <div>
+              <p>Login</p>
+              <button className="login-btn" onClick={this.login}>Login</button>
+          </div>
+      )
+
+
+      return (
+          <div className="profileNav">
+              <Nav />
+              {button}
+          </div>
+      )
+  }
+})
